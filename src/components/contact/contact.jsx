9c9fdd94e69ce513e12f36e7574bb3f41644889a
Sukumar Biswas <sukumarbsws98@gmail.com>
2026-06-06 14:25:54 +0600

    contact section fixing

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -59,11 +59,11 @@ const Contact = () => {
                 </div>
             </div>
             <form onSubmit={onSubmit} className="contact-right">
-                <label htmlFor="">Your Name</label>
+                {/* <label htmlFor="">Your Name</label> */}
                 <input type="text" placeholder='Enter your name' name='name'/>
-                <label htmlFor="">Your Email</label>
+                {/* <label htmlFor="">Your Email</label> */}
                 <input type="email" placeholder='Enter your email' name='email'/>
-                <label htmlFor="">Write your message here</label>
+                {/* <label htmlFor="">Write your message here</label> */}
                 <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
                 <button type='submit' className='contact-submit'>Submit Now</button>
             </form>
